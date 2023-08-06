58a41d191d414ea5c1f93761a811748c8c8f83d0
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-08-06 16:52:43 +0530

    Update SQL2.sql

--- WSDA_Music/Challenge1/SQL2.sql
+++ WSDA_Music/Challenge1/SQL2.sql
@@ -1,1 +1,5 @@
+-- Challenge 1
+-- How much money did WSDA Music make during the same period?
+
+
 SELECT SUM(TOTAL) AS 'TOTAL MONEY' FROM Invoice WHERE InvoiceDate LIKE '2011%' OR InvoiceDate LIKE '2012%'
